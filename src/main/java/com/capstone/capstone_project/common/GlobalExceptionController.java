2d49453775a76de8c0eed367eb6b4be9e4f75006
jungmyung <jungmyung30830@gmail.com>
2024-05-21 00:50:25 +0900

    멤버 예외 처리를 하기 위한 전역 예외 컨트롤러

--- src/main/java/com/capstone/capstone_project/common/GlobalExceptionController.java
+++ src/main/java/com/capstone/capstone_project/common/GlobalExceptionController.java
@@ -4,6 +4,10 @@ import com.capstone.capstone_project.common.exception.MemberException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+/*
+ * 멤버 예외 처리를 하기 위한 전역 예외 컨트롤러
+ */
+
 public class GlobalExceptionController {
     @ExceptionHandler(MemberException.class)
     public ResponseEntity<String> handleMemberException(MemberException e) {
