389e269a5faca5a687d46c69ac96f769c8b078f5
jungmyung <jungmyung30830@gmail.com>
2024-05-21 02:02:35 +0900

    Spring Security를 설정하는 데 사용, JWT 기반의 사용자 인증 및 권한 부여가 포함

--- src/main/java/com/capstone/capstone_project/security/SecurityConfig.java
+++ src/main/java/com/capstone/capstone_project/security/SecurityConfig.java
@@ -15,6 +15,10 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+/*
+ *  Spring Security를 설정하는 데 사용, JWT 기반의 사용자 인증 및 권한 부여가 포함
+ */
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -23,12 +27,14 @@ public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final CorsConfigurationSource corsConfigurationSource;
 
+    // Spring Security의 AuthenticationManager를 빈으로 설정하여 사용자의 인증을 관리
     @Bean
     public AuthenticationManager authenticationManager(
             AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
+    // HTTP 보안 구성을 정의합니다. HTTP 기본 설정, CSRF 보호, CORS 구성 등을 포함
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
