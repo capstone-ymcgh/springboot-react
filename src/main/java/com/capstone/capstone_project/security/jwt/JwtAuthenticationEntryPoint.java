698de90920f8081c5f4d05492d3cdc29ba929248
jungmyung <jungmyung30830@gmail.com>
2024-05-21 01:52:31 +0900

    인증되지 않은 사용자가 보호된 리소스에 액세스하려고 할 때 발생하는 예외를 처리

--- src/main/java/com/capstone/capstone_project/security/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/capstone/capstone_project/security/jwt/JwtAuthenticationEntryPoint.java
@@ -1,5 +1,10 @@
 package com.capstone.capstone_project.security.jwt;
 
+/*
+ * 인증되지 않은 사용자가 보호된 리소스에 액세스하려고 할 때 발생하는 예외를 처리하는 데 사용.
+ * 여기서는 SC_UNAUTHORIZED(401 Unauthorized) 상태 코드를 응답으로 보내고 "Unauthorized" 메시지를 함께 전송
+ */
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
