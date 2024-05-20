c0ef49e5e93a839e1e867be871041c68879ca763
jungmyung <jungmyung30830@gmail.com>
2024-05-21 02:25:22 +0900

    시간 정보(createDate와 modifiedDate)를 자동으로 처리하기 위한 기본 엔티티 클래스

--- src/main/java/com/capstone/capstone_project/common/BaseTimeEntity.java
+++ src/main/java/com/capstone/capstone_project/common/BaseTimeEntity.java
@@ -13,7 +13,7 @@ import java.time.format.DateTimeFormatter;
 /*
 시간 정보(createDate와 modifiedDate)를 자동으로 처리하기 위한 기본 엔티티 클래스이다.
 다른 엔티티 클래스가 상속받아 사용할 수 있게 하기 위함.
-엔티티가 생성되거나 수정될 때마다 자동으로 타임스탬프가 업데이트 된다.
+엔티티가 생성되거나 수정될 때마다 자동으로 타임스탬프가 업데이트 된다
  */
 @Getter
 @MappedSuperclass
